26332c35f47b1cf703165831e2f9a8bb79ff910e
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2026-05-26 16:26:20 +0530

    fix: ui for chat component layout, create chat id page with onClick function in layout component

---
+++ app/(protected)/chat/[chatId]/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page({params}: { params: Promise<{ chatId: string }> }) {
+    const { chatId } = await params;
+    return (
+        <div>
+            {chatId}
+        </div>
+    )
+}
--- components/ChatLayoutComponent.tsx
+++ components/ChatLayoutComponent.tsx
@@ -1,18 +1,21 @@
-import { SquareTerminal } from "lucide-react";
+"use client";
 
-export function ChatLayoutComponent(){
+import { Plus } from "lucide-react";
+import { redirect, usePathname } from "next/navigation";
+
+export function ChatLayoutComponent() {
+    const path = usePathname(); // example path: /chat or /chat/1
     return (
         <div>
-            <div className="flex items-center justify-between w-full">
-                <p className="text-gray-800 text-sm">Conversations</p>
-                <button className="cursor-pointer border border-gray-400 py-0.5 px-2 text-sm rounded-md">
-                    + New
-                </button>
+            <div className="w-full">
+                <div className="flex gap-2">
+                    <Plus size={18} className="" />
+                    <p className="text-gray-700 text-sm">New Chat</p>
+                </div>
             </div>
             <div>
                 <div className="flex gap-2 items-center mt-4 p-2 cursor-pointer bg-bg_secondary">
-                    <SquareTerminal className="text-icon_primary" size={14} />
-                    <p className="font-semibold text-sm">Codebot Session 1</p>
+                    <p onClick={() => redirect("/chat/1")} className="font-semibold text-sm">Session 1</p>
                 </div>
             </div>
         </div>
